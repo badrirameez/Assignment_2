339fa017df5d90d8564bcd3dcd382b1c89e8fc2f
rameezbadri <rameezbadri@usf.edu>
2020-06-22 11:31:35 -0400

    Initial commit

---
+++ Assignment_2.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Assignment_2
+{
+    class Assignment_2
+    {
+        static void Main(string[] args)
+        {
+            int[] numberArray = new int[10];
+            numberArray[0] = 0;
+            numberArray[1] = 1;
+            numberArray[2] = 2;
+            numberArray[3] = 3;
+            numberArray[4] = 4;
+            numberArray[5] = 5;
+            numberArray[6] = 6;
+            numberArray[7] = 7;
+            numberArray[8] = 8;
+            numberArray[9] = 9;
+            numberArray[10] = 10;
+
+
+        }
+    }
+}
